037c61665dd4e48fb46c78435cb7cfd81523cee7
Miles Zhang <mingchang555@hotmail.com>
2020-09-24 16:28:18 +0800

    import ant-design-vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <Auth/>
+<a-layout id="components-layout-demo-responsive">
+    <a-layout-sider
+      breakpoint="lg"
+      collapsed-width="0"
+      @collapse="onCollapse"
+      @breakpoint="onBreakpoint"
+    >
+      <div class="logo">
+        <span>
+        <ThunderboltOutlined />
+        Thunder Issue SUDT
+        </span>
+      </div>
+      <a-menu theme="dark" mode="inline" v-model:selectedKeys="selectedKeys">
+        <a-menu-item key="1">
+          <home-outlined />
+          <span class="nav-text">Wallet Info</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header :style="{ background: '#fff', padding: 0 }" />
+      <a-layout-content :style="{ margin: '24px 16px 0'}">
+        <div :style="{ padding: '24px', background: '#fff', height: '100%' }">
+          <Auth />
+        </div>
+      </a-layout-content>
+      <a-layout-footer style="textAlign: center">
+        Thunder Issue SUDT ©2020 Created by Nervos
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import Auth from './components/Auth.vue'
+import { Options, Vue } from "vue-class-component"
+import Auth from "./components/Auth.vue"
+import {
+  HomeOutlined,
+  ThunderboltOutlined
+} from '@ant-design/icons-vue'
 
 @Options({
   components: {
-    Auth
+    Auth,
+    HomeOutlined,
+    ThunderboltOutlined
   }
 })
-export default class App extends Vue {}
+
+export default class App extends Vue {
+  data () {
+    return {
+      selectedKeys: ['1']
+    }
+  }
+}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
+.ant-layout {
+  height: 100%;
+}
+#components-layout-demo-responsive .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { Layout, Menu } from 'ant-design-vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(Layout)
+app.use(Menu)
+app.mount("#app")
