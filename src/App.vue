9c2e237246b045abc65ef09becfc8182e7fcf280
Miles Zhang <mingchang555@hotmail.com>
2020-12-17 10:46:19 +0800

    style: set locale element bottom

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,53 @@
       <div class="logo">
         <span>
           <ThunderboltFilled style="color: yellow" />
-            {{ $t("title.root")}}
+          {{ $t('title.root') }}
+        </span>
+      </div>
+      <a-divider />
+      <a-menu theme="dark" mode="inline" :selected-keys="[this.$route.path]" style="height: 72%;">
+        <a-menu-item key="/">
+          <router-link to="/">
+            <HomeFilled />
+            <span class="nav-text">{{ $t('menu.auth') }}</span>
+          </router-link>
+        </a-menu-item>
+        <a-menu-item key="/issue">
+          <router-link to="/issue">
+            <MoneyCollectFilled />
+            <span class="nav-text">{{ $t('menu.issue') }}</span>
+          </router-link>
+        </a-menu-item>
+        <a-menu-item key="/transfer">
+          <router-link to="/transfer">
+            <InteractionFilled />
+            <span class="nav-text">{{ $t('menu.transfer') }}</span>
+          </router-link>
+        </a-menu-item>
+        <a-menu-item key="/burn">
+          <router-link to="/burn">
+            <FireFilled />
+            <span class="nav-text">{{ $t('menu.burn') }}</span>
+          </router-link>
+        </a-menu-item>
+        <a-menu-item key="/info">
+          <router-link to="/info">
+            <InfoCircleFilled />
+            <span class="nav-text">{{ $t('menu.info') }}</span>
+          </router-link>
+        </a-menu-item>
+      </a-menu>
+      <a-divider />
+      <a-row>
+        <a-col :span="12" :offset="3">
           <a-dropdown>
             <a class="ant-dropdown-link" @click="(e) => e.preventDefault()">
-              {{ locale }}<DownOutlined />
+              <a-space>
+              <GlobalOutlined />
+              {{ locale }}
+              </a-space>
+              <DownOutlined />
+
             </a>
             <template #overlay>
               <a-menu @click="switchLocale">
@@ -20,41 +63,8 @@
               </a-menu>
             </template>
           </a-dropdown>
-        </span>
-      </div>
-      <a-divider />
-      <a-menu theme="dark" mode="inline" :selected-keys="[this.$route.path]">
-        <a-menu-item key="/">
-          <router-link to="/">
-            <HomeFilled />
-            <span class="nav-text">{{ $t("menu.auth") }}</span>
-          </router-link>
-        </a-menu-item>
-        <a-menu-item key="/issue">
-          <router-link to="/issue">
-            <MoneyCollectFilled />
-            <span class="nav-text">{{ $t("menu.issue") }}</span>
-          </router-link>
-        </a-menu-item>
-        <a-menu-item key="/transfer">
-          <router-link to="/transfer">
-            <InteractionFilled />
-            <span class="nav-text">{{ $t("menu.transfer") }}</span>
-          </router-link>
-        </a-menu-item>
-        <a-menu-item key="/burn">
-          <router-link to="/burn">
-            <FireFilled />
-            <span class="nav-text">{{ $t("menu.burn") }}</span>
-          </router-link>
-        </a-menu-item>
-        <a-menu-item key="/info">
-          <router-link to="/info">
-            <InfoCircleFilled />
-            <span class="nav-text">{{ $t("menu.info") }}</span>
-          </router-link>
-        </a-menu-item>
-      </a-menu>
+        </a-col>
+      </a-row>
     </a-layout-sider>
     <a-layout>
       <a-layout-content :style="{ margin: '24px 16px 0' }">
@@ -79,7 +89,8 @@ import {
   FireFilled,
   MoneyCollectFilled,
   InfoCircleFilled,
-  DownOutlined
+  DownOutlined,
+  GlobalOutlined
 } from '@ant-design/icons-vue'
 
 export default defineComponent({
@@ -90,7 +101,8 @@ export default defineComponent({
     FireFilled,
     MoneyCollectFilled,
     InfoCircleFilled,
-    DownOutlined
+    DownOutlined,
+    GlobalOutlined
   },
   data() {
     return {
