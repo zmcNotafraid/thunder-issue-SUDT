2ff43da4bb0304f9153f38f56630c3e1baa82038
Miles Zhang <mingchang555@hotmail.com>
2020-12-29 23:01:13 +0800

    style: add notice in info page

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -2,7 +2,9 @@
   <div>
     <a-row>
       <a-col :span="8">
-        <a-button type="primary" @click="showModal"> {{ $t('buttons.updateTokenInfo') }} </a-button>
+        <a-button type="primary" @click="showModal">
+          {{ $t('buttons.updateTokenInfo') }}
+        </a-button>
       </a-col>
       <a-col :span="3" :offset="13">
         <a href="javascript:void(0);" @click="submitInfo">
@@ -10,18 +12,33 @@
         </a>
       </a-col>
     </a-row>
+  </div>
+  <br/>
+  <a-card :title="$t('title.notice')" size="large" style="color: #faad14">
+    <ol type="1">
+      <li>{{ $t('warning.infoFirst') }}</li>
+      <li>{{ $t('warning.infoSecond') }}</li>
+      <li>{{ $t('warning.infoThird') }}</li>
+    </ol>
+  </a-card>
+  <div>
     <a-modal
       :title="$t('title.updateSudtInfo')"
       :visible="visible"
       @cancel="handleCancel"
       :confirm-loading="confirmLoading"
-      :footer=true
+      :footer="true"
     >
-      <component-sudt-form ref="sudtInfoForm" :issue-sudt=false :label-column=6 :wrapper-column=10></component-sudt-form>
+      <component-sudt-form
+        ref="sudtInfoForm"
+        :issue-sudt="false"
+        :label-column="6"
+        :wrapper-column="10"
+      ></component-sudt-form>
       <a-row>
-        <a-col :span='4' :offset="6">
+        <a-col :span="4" :offset="6">
           <a-button key="submit" type="primary" @click="handleSubmit">
-            {{ $t("buttons.submit") }}
+            {{ $t('buttons.submit') }}
           </a-button>
         </a-col>
       </a-row>
@@ -36,7 +53,7 @@
       {{ symbol }}
     </a-descriptions-item>
     <a-descriptions-item :label="$t('labels.tokenDecimal')">
-      {{ decimal === -1 ? "" : decimal }}
+      {{ decimal === -1 ? '' : decimal }}
     </a-descriptions-item>
     <a-descriptions-item :label="$t('labels.tokenIssuer')">
       {{ address }}
@@ -73,21 +90,26 @@ export default defineComponent({
   async mounted() {
     const authToken: string | null = window.localStorage.getItem('authToken')
     if (!authToken) {
-      message.error(this.$t("errors.noAuth"))
+      message.error(this.$t('errors.noAuth'))
       return
     }
     if (!(await isKeyperingConnected(this))) {
-      this.name = ""
-      this.symbol = ""
+      this.name = ''
+      this.symbol = ''
       this.decimal = -1
-      this.address = ""
+      this.address = ''
       return
     }
 
-    const sudtInfoTypeScript = getNetworkConst("SUDT_INFO_TYPE_SCRIPT") as CKBComponents.Script
-    sudtInfoTypeScript.args = window.localStorage.getItem('lockHash') || ""
+    const sudtInfoTypeScript = getNetworkConst(
+      'SUDT_INFO_TYPE_SCRIPT'
+    ) as CKBComponents.Script
+    sudtInfoTypeScript.args = window.localStorage.getItem('lockHash') || ''
 
-    const cells = await getCells('type', underscoreScriptKey(sudtInfoTypeScript))
+    const cells = await getCells(
+      'type',
+      underscoreScriptKey(sudtInfoTypeScript)
+    )
     if (cells.length === 0) {
       return
     }
@@ -116,24 +138,26 @@ export default defineComponent({
       return (this.$refs.sudtInfoForm as HTMLFormElement).checkFormValidate()
     },
     submitInfo() {
-      const sudtTypeScript = getNetworkConst("SUDT_TYPE_SCRIPT") as CKBComponents.Script
-      const sudtTypeArgs = window.localStorage.getItem("lockHash") || ""
+      const sudtTypeScript = getNetworkConst(
+        'SUDT_TYPE_SCRIPT'
+      ) as CKBComponents.Script
+      const sudtTypeArgs = window.localStorage.getItem('lockHash') || ''
       const TOKEN_EMAIL_BODY = `
-                        Title: Submit Token Information%0a%0d
-                        Type Script:%0a%0d
-                            Code Hash: ${sudtTypeScript.codeHash}%0a%0d
-                            Hash Type: ${sudtTypeScript.hashType}%0a%0d
-                            Args: ${sudtTypeArgs}%0a%0d
-                        Information:%0a%0d
-                          Full Name: ${this.name}%0a%0d
-                          Symbol: ${this.symbol}%0a%0d
-                          Decimal: ${this.decimal}%0a%0d
-                          Description:%0a%0d
-                          Website:%0a%0d
-                          Icon File: attachment (40 x 40)%0a%0d
-                          Other Info:%0a%0d
-                        Note: Only accept sUDT information submission now.
-                        `
+                          Title: Submit Token Information%0a%0d
+                          Type Script:%0a%0d
+                              Code Hash: ${sudtTypeScript.codeHash}%0a%0d
+                              Hash Type: ${sudtTypeScript.hashType}%0a%0d
+                              Args: ${sudtTypeArgs}%0a%0d
+                          Information:%0a%0d
+                            Full Name: ${this.name}%0a%0d
+                            Symbol: ${this.symbol}%0a%0d
+                            Decimal: ${this.decimal}%0a%0d
+                            Description:%0a%0d
+                            Website:%0a%0d
+                            Icon File: attachment (40 x 40)%0a%0d
+                            Other Info:%0a%0d
+                          Note: Only accept sUDT information submission now.
+                          `
       open(
           `mailto:asset-info-submit@nervos.org?subject=Submit Token Info&body=${TOKEN_EMAIL_BODY}`
       )
